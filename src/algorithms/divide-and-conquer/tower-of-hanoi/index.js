329bd51034013718dbeae3467d27bac339c536d1
adityanichkawade <aditya.nichkawade@gmail.com>
2020-07-05 23:03:05 +0530

    Added the comments in tower of hanoi

--- src/algorithms/divide-and-conquer/tower-of-hanoi/index.js
+++ src/algorithms/divide-and-conquer/tower-of-hanoi/index.js
@@ -4,8 +4,11 @@ const moveDisks = (sourcePeg, auxillaryPeg, destinationPeg, numOfDisks, callback
     return;
   }
 
+  // Move n-1 disk from source peg to auxilary peg
   moveDisks(sourcePeg, destinationPeg, auxillaryPeg, numOfDisks - 1, callback);
+  // Move the bottom disk from source peg to destination peg
   moveDisks(sourcePeg, auxillaryPeg, destinationPeg, 1, callback);
+  // Move n-1 disk from auxilary peg to desination peg
   moveDisks(auxillaryPeg, sourcePeg, destinationPeg, numOfDisks - 1, callback);
 };
 
