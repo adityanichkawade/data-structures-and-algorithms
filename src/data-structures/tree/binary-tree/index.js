88567c12768ce3596a0b0e5c2ed3330f865fe4f6
adityanichkawade <aditya.nichkawade@gmail.com>
2020-07-10 23:13:37 +0530

    Added traversing for binary tree

--- src/data-structures/tree/binary-tree/index.js
+++ src/data-structures/tree/binary-tree/index.js
@@ -1,5 +1,17 @@
 import BinaryTreeNode from './BinaryTreeNode';
 
+function getCorrectNode(node, newNode) {
+  return newNode.data < node.data ? node.leftNode : node.rightNode;
+}
+
+function setCorrectNode(node, newNode) {
+  if (node.data > newNode.data) {
+    node.setLeft(newNode);
+  } else {
+    node.setRight(newNode);
+  }
+}
+
 class BinaryTree {
   constructor(data) {
     this.rootNode = new BinaryTreeNode(data);
@@ -15,16 +27,54 @@ class BinaryTree {
   }
 
   addNode(node, newNode) {
-    if (newNode.data < node.data) {
-      if (node.leftNode) {
-        this.addNode(node.leftNode, newNode);
-      } else {
-        node.setLeft(newNode);
-      }
-    } else if (node.rightNode) {
-      this.addNode(node.rightNode, newNode);
+    const correctNode = getCorrectNode(node, newNode);
+    if (correctNode) {
+      this.addNode(correctNode, newNode);
     } else {
-      node.setRight(newNode);
+      setCorrectNode(node, newNode);
+    }
+  }
+
+  traverseTree(callback, type) {
+    const traversal = {
+      PreOrder: () => {
+        this.preOrder(this.rootNode, callback);
+      },
+      InOrder: () => {
+        this.inOrder(this.rootNode, callback);
+      },
+      PostOrder: () => {
+        this.postOrder(this.rootNode, callback);
+      },
+    };
+
+    const traversalFunc = traversal[type];
+    if (traversalFunc) {
+      traversalFunc();
+    }
+  }
+
+  preOrder(root, callback) {
+    if (root) {
+      callback(root);
+      this.preOrder(root.leftNode);
+      this.preOrder(root.rightNode);
+    }
+  }
+
+  inOrder(root, callback) {
+    if (root) {
+      this.inOrder(root.leftNode);
+      callback(root);
+      this.inOrder(root.rightNode);
+    }
+  }
+
+  postOrder(root, callback) {
+    if (root) {
+      this.postOrder(root.leftNode);
+      this.postOrder(root.rightNode);
+      callback(root);
     }
   }
 }
